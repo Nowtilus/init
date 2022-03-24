069f67fcca62152d16f38adb74e70290cc239d33
motokotoboom@gmail.com <motokotoboom@gmail.com>
2022-03-24 16:11:41 +0100

    nginx binary with metrics

--- init.sh
+++ init.sh
@@ -2,8 +2,14 @@
 mkdir /mnt/ramdisk
 mount -t tmpfs -o size=25G ramdisk /mnt/ramdisk
 apt-get update
-apt-get -y install nfs-kernel-server
+apt-get -y install nfs-kernel-server wget nginx
 echo "/mnt/ramdisk/ 10.0.0.0/8(rw,async,no_subtree_check,anonuid=33,anongid=33,fsid=42)" >> /etc/exports
 systemctl restart nfs-kernel-server
+wget https://github.com/Nowtilus/init/raw/master/clean.sh -O /usr/bin/clean.sh
+wget https://github.com/Nowtilus/init/blob/master/nginx/default -O /etc/nginx/sites-enabled
+service nginx restart
+echo "* * * * * sh /urs/bin/clean.sh > /etc/crontab/cleanramdisk"
+
+
 #* * * * * sudo find /mnt/ramdisk/das-2-ssai/media -type f -mmin +120 -delete
 #* * * * * sudo find /mnt/ramdisk/das-2-ssai/vast -type f -mmin +120 -delete
